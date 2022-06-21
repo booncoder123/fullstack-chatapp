48241382fd345add9b2fb70777a024b744a6809e
booncoder <chanok_boon@hotmail.com>
2022-06-21 23:26:44 +0700

    typo

--- components/Bottombar.js
+++ components/Bottombar.js
@@ -1,29 +1,51 @@
 import { useState } from "react";
 import { FormControl, Input, Button } from "@chakra-ui/react";
-import { serverTimestamp, addDoc, collection } from "firebase/firestore";
+import {
+  serverTimestamp,
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebaseconfig";
 
-export default function Bottombar({id, user}) {
+export default function Bottombar({ id, user }) {
   const [input, setInput] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    await addDoc(collection(db, `chats/${id}/messages`), {
-      text: input,
-      sender: user.email,
-      timestamp: serverTimestamp()
-    })
-    setInput("");
-  }
+    try {
+      console.log("Sending message");
+      await addDoc(collection(db, `chats/${id}/messages`), {
+        text: input,
+        sender: user.email,
+        timestamp: serverTimestamp(),
+      });
+
+      await onSnapshot(collection(db, `chats/${id}/messages`), (doc) => {
+        const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+        // console.log(source, " data: ", doc.data());
+        console.log(source);
+      });
+
+      setInput("");
+    } catch (error) {
+      console.log("error na kub");
+      console.log({ error });
+    }
+  };
 
   return (
-    <FormControl
-      p={3}
-      onSubmit={sendMessage}
-      as="form"
-    >
-      <Input placeholder="Type a message..." autoComplete="off" onChange={e => setInput(e.target.value)} value={input} />
-      <Button type="submit" hidden>Submit</Button>
+    <FormControl p={3} onSubmit={sendMessage} as="form">
+      <Input
+        placeholder="Type a message..."
+        autoComplete="off"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button type="submit" hidden>
+        Submit
+      </Button>
     </FormControl>
-  )
+  );
 }
--- firebaseconfig.js
+++ firebaseconfig.js
@@ -5,17 +5,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "INSERT DETAILS HERE",
-  authDomain: "INSERT DETAILS HERE",
-  projectId: "INSERT DETAILS HERE",
-  storageBucket: "INSERT DETAILS HERE",
-  messagingSenderId: "INSERT DETAILS HERE",
-  appId: "INSERT DETAILS HERE"
+  apiKey: "AIzaSyBsJLz9JRFaincv8Os6jeFGyDE8bONGxUA",
+  authDomain: "poc-chat-app-533db.firebaseapp.com",
+  projectId: "poc-chat-app-533db",
+  storageBucket: "poc-chat-app-533db.appspot.com",
+  messagingSenderId: "557402318028",
+  appId: "1:557402318028:web:4c21c61b6c04e1fceb0e69",
+  measurementId: "G-TTJ4Y2GZ2N",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getFirestore();
+const db = getFirestore(app);
 
-export { auth, db }
+export { auth, db };
